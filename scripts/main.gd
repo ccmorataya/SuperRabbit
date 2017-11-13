a9508c953fe17e2420e2e30c2be0a081afad0948
Cristian Morataya <cris.morataya@gmail.com>
2017-11-12 23:48:03 -0600

    Add spritesheet and organize source sprites

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,19 +8,24 @@ func _ready():
 	#	print(rabbit.get_type())
 
 func _fixed_process(delta):
+	var rabbitSprite = rabbit.get_node("SuperRabitRPGstyle")
 	var rabbitDirection = Vector2(0,0)
 	
 	if (Input.is_action_pressed("ui_right")):
 		print("right")
+		rabbitSprite.set_frame(0)
 		rabbitDirection += Vector2(1,0)
 	elif (Input.is_action_pressed("ui_left")):
 		print("left")
+		rabbitSprite.set_frame(1)
 		rabbitDirection += Vector2(-1,0)
 	elif (Input.is_action_pressed("ui_up")):
 		print("up")
 		rabbitDirection += Vector2(0,-1)
+		rabbitSprite.set_frame(2)
 	elif (Input.is_action_pressed("ui_down")):
 		print("down")
+		rabbitSprite.set_frame(3)
 		rabbitDirection += Vector2(0,1)
 	
 	var movement = rabbitDirection * delta * speed
