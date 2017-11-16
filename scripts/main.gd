2ddfe480d475c8fce66ed14f09238652c5296722
Cristian Morataya <cris.morataya@gmail.com>
2017-11-15 21:04:29 -0600

    Add rabbit movement sound

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 onready var rabbit = get_node("kb2Rabbit")
+onready var rabbitWalkSound = rabbit.get_node("smpSRwalk")
 onready var speed = 400
 
 func _ready():
 	set_fixed_process(true)
+	set_process_input(true)
 	#	print(rabbit.get_type())
 
 func _fixed_process(delta):
@@ -12,22 +14,27 @@ func _fixed_process(delta):
 	var rabbitDirection = Vector2(0,0)
 	
 	if (Input.is_action_pressed("ui_right")):
-		print("right")
 		rabbitSprite.set_frame(0)
 		rabbitDirection += Vector2(1,0)
 	elif (Input.is_action_pressed("ui_left")):
-		print("left")
 		rabbitSprite.set_frame(1)
 		rabbitDirection += Vector2(-1,0)
 	elif (Input.is_action_pressed("ui_up")):
-		print("up")
-		rabbitDirection += Vector2(0,-1)
 		rabbitSprite.set_frame(2)
+		rabbitDirection += Vector2(0,-1)
 	elif (Input.is_action_pressed("ui_down")):
-		print("down")
 		rabbitSprite.set_frame(3)
 		rabbitDirection += Vector2(0,1)
 	
 	var movement = rabbitDirection * delta * speed
 	rabbit.move(movement)
-	print(movement)
+	var parameter = Vector2(0,0)
+	# CM => TODO: adjust stop_all() instant to match sample end
+	if movement == parameter:
+		rabbitWalkSound.stop_all()
+
+func _input(event):
+	if event.is_action_pressed("ui_right") || event.is_action_pressed("ui_left") || event.is_action_pressed("ui_up") || event.is_action_pressed("ui_down"):
+		rabbitWalkSound.play("srWalk")
+	#elif event.is_action_released("ui_right") || event.is_action_released("ui_left") || event.is_action_released("ui_up") || event.is_action_released("ui_down"):
+	#	rabbitWalkSound.stop_all()
